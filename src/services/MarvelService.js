53556f78d5c56a56d864f6d1d6425799aacea6f0
inne <mrbelongtim@gmail.com>
2024-02-01 13:07:23 +0100

    added queries to server for random char

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,10 @@
 
 
 class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=1479d30485ed34b10707b0c2faaf9ffe';
+
     getResource = async (url) => {
         let res = await fetch (url);
 
@@ -12,7 +16,23 @@ class MarvelService {
     }
 
     getAllCharacters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=1479d30485ed34b10707b0c2faaf9ffe');
+        const res = this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async(id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
